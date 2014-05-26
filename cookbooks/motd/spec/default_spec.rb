cb58476513a119df05c214c5ecba498f1ed014a4
Daniel Borcherding <daniel.borcherding@gmail.com>
2014-05-25 17:42:45 -0700

    Added minutest to check to see that the etc exists

--- cookbooks/motd/spec/default_spec.rb
+++ cookbooks/motd/spec/default_spec.rb
@@ -1,10 +1,12 @@
 require 'chefspec'
 
 describe 'motd::default' do
-  let (:chef_run) { ChefSpec::ChefRunner.new.converge 'motd::default' }
+  let(:chef_run) { ChefSpec::Runner.new }
   it 'should do create a motd file with the company name' do
     pending 'Your recipe examples go here.'
-    #Chef::Recipe.any_instance.stub(:data_bag_item).with('global-properties', 'company').and_return({'id' => 'company', 'name' => 'Never Stop Building'})
-    #expect(chef_run).to create_file_with_content('/etc/motd.tail', 'This Server is the Property of Never Stop Building')
+  end
+
+  before do
+    stub_data_bag('users').and_return([])
   end
 end
